98d76c52ee7cb477b71ad20428ca01b476d25025
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2022-10-18 17:46:00 +0300

    homework
    sql northwind

---
+++ odev5/sqlnorthwind/sqlnorthwind/SqlQuery_1.sql
@@ -0,0 +1,31 @@
+﻿sql--Select
+--ANSII
+Select ContactName Adi,CompanyName Sirketadi,City Sehir from Customers
+
+Select * from Customers where City = 'Berlin'
+
+--case insensitive
+select * from Products where CategoryID=1 or CategoryID=3
+
+select * from Products where CategoryID=1 and UnitPrice>=10
+
+select * from Products where CategoryID=1 order by UnitPrice desc --ascending (artan)  --desc descending(düşen)
+
+select count(*) Adet from Products
+
+select CategoryID,COUNT(*) from Products where UnitPrice>20 group by CategoryID having count(*)<10
+
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName 
+from Products inner join Categories 
+on Products.CategoryID = Categories.CategoryID
+where Products.UnitPrice>10
+
+select * from Products p inner join [Order Details] od
+on p.ProductID = od.ProductID
+inner join Orders o
+on o.OrderID = od.OrderID
+
+select * from Customers c left join Orders o
+on c.CustomerID = o.CustomerID
+where o.CustomerID is null
+
